ec4c20390ec3561abc6bce3c58608afa5b9ae10d
omegaga <omegaleedh@gmail.com>
2015-07-01 23:30:02 +0800

    initial commit

---
+++ downloader.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+from fetch import fetch_video
+from urllib2 import urlopen, HTTPError
+import json
+from sys import argv, exit, stderr
+from getopt import getopt
+
+
+# folder id of 15-213 Introduction to Computer Systems, change if necessary
+CSAPP_FOLDER_ID = '2be55024-d2d3-4a3c-ac9c-9b348ddce5c4'
+
+def read_args(argv):
+    try:
+        opts, args = getopt(argv[1:], 'ah')
+    except getopt.GetoptError:
+        usage()
+        exit(1)
+
+    all_flag = False
+
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            exit(0)
+        elif opt == '-a':
+            all_flag = True
+
+    return all_flag
+
+def usage():
+    print 'Usage: %s [-a]' % argv[0]
+
+def get_sessions(folder_id):
+    url = 'https://scs.hosted.panopto.com/Panopto/PublicAPI/4.1/ListSessions'\
+        + '?FolderId=%s' % folder_id
+    req = urlopen(url)
+    res = json.loads(req.read())
+    video_list = [(e['Id'], '%s.mp4' % e['Name']) for e in res['Results']]
+    return video_list
+
+if __name__ == '__main__':
+    all_flag = read_args(argv)
+    video_list = get_sessions(CSAPP_FOLDER_ID)
+    idx = 0
+    if all_flag:
+        for video in video_list:
+            try:
+                fetch_video(video[1], video[0])
+            except HTTPError:
+                stderr.write('Error: fail to download %s\n' % video[1])
+        exit(0)
+
+    for idx in xrange(len(video_list)):
+        print '%d) %s' % (idx, video_list[idx][1])
+
+    idx = input('Please input the index of the video to download: ')
+    video = video_list[idx]
+    fetch_video(video[1], video[0])
---
+++ fetch.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import json
+from sys import argv, stdout, exit
+from urllib2 import urlopen
+from getopt import getopt
+
+CHUNK = 16 * 1024
+
+def read_args(argv):
+    filename = None
+    uuid = None
+
+    try:
+        opts, args = getopt(argv[1:], 'u:o:h')
+    except getopt.GetoptError:
+        usage()
+        exit(1)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            exit(0)
+        elif opt == '-o':
+            filename = arg
+        elif opt == '-u':
+            uuid = arg
+        else:
+            usage()
+            exit(1)
+
+    if not filename or not uuid:
+        usage()
+        exit(1)
+    return filename, uuid
+
+def usage():
+    print 'Usage: %s -u uuid -o file' % argv[0]
+
+def fetch_video_url(uuid):
+    url =\
+      'http://scs.hosted.panopto.com/Panopto/PublicAPI/4.1/TabletDeliveryInfo'\
+      + '?DeliveryId=%s&forDownload=true' % uuid
+    req = urlopen(url)
+    res = json.loads(req.read())
+    return res['PhoneDownloadUrl']
+
+def download_video(video_url, filename):
+    video_req = urlopen(video_url)
+    chunk_count = 0
+    total_length = float(video_req.headers.get('content-length'))
+    with open(filename, 'wb') as fp:
+        while True:
+            percent = 100.0 * chunk_count * CHUNK / total_length
+            stdout.write("\r%.2f%%" % percent)
+            stdout.flush()
+            chunk = video_req.read(CHUNK)
+            if not chunk:
+                break
+            fp.write(chunk)
+            chunk_count += 1
+
+def fetch_video(filename, uuid):
+    print 'Fetching video %s' % filename
+    print 'Fetching video url...'
+    video_url = fetch_video_url(uuid)
+    print 'video url: %s' % video_url
+
+    print 'Downloading %s' % filename
+    download_video(video_url, filename)
+
+    print ''
+    print 'Done!'
+
+if __name__ == '__main__':
+    filename, uuid = read_args(argv)
+    fetch_video(filename, uuid)
